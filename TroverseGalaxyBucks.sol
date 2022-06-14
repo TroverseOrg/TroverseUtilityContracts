baf38d0e794da6494259a08e567c6f36f134ea82
TroverseDev <troversenft@gmail.com>
2022-06-14 10:58:20 -0700

    Fixed: refillPolygonToken()

--- TroverseGalaxyBucks.sol
+++ TroverseGalaxyBucks.sol
@@ -23,17 +23,18 @@ contract TroverseGalaxyBucks is ERC20, Ownable {
     mapping(address => bool) public operators;
     address public manager;
 
-    address public erc20Predicate;
+    address public erc20PredicateProxy;
+    address public erc20Polygon;
     address public erc20PolygonManager;
 
     IRootChainManager public rootChainManager;
 
     constructor(
-        address _erc20Predicate,
+        address _erc20PredicateProxy,
         address _erc20PolygonManager,
         IRootChainManager _rootChainManager
     ) ERC20("Troverse Galaxy Bucks", "G-Bucks") {
-        erc20Predicate = _erc20Predicate;
+        erc20PredicateProxy = _erc20PredicateProxy;
         erc20PolygonManager = _erc20PolygonManager;
         rootChainManager = _rootChainManager;
     }
@@ -72,45 +73,32 @@ contract TroverseGalaxyBucks is ERC20, Ownable {
     }
 
     function refillPolygonToken(uint256 _amount) external onlyOwner {
-        _mint(_msgSender(), _amount);
-        approve(erc20Predicate, _amount);
+        _mint(address(this), _amount);
+        approve(erc20PredicateProxy, _amount);
         rootChainManager.depositFor(
             erc20PolygonManager,
-            address(this),
+            erc20Polygon,
             abi.encode(_amount)
         );
     }
 
-    function updateErc20Predicate(address _newErc20Predicate)
-        external
-        onlyOwner
-    {
-        require(
-            _newErc20Predicate != address(0),
-            "Bad Erc20Predicate address"
-        );
-        erc20Predicate = _newErc20Predicate;
+    function updateErc20PredicateProxy(address _newErc20PredicateProxy) external onlyOwner {
+        require(_newErc20PredicateProxy != address(0), "Bad Erc20PredicateProxy address");
+        erc20PredicateProxy = _newErc20PredicateProxy;
     }
 
-    function updateRootChainManager(IRootChainManager _newRootChainManager)
-        external
-        onlyOwner
-    {
-        require(
-            address(_newRootChainManager) != address(0),
-            "Bad RootChainManager address"
-        );
+    function updateRootChainManager(IRootChainManager _newRootChainManager) external onlyOwner {
+        require(address(_newRootChainManager) != address(0), "Bad RootChainManager address");
         rootChainManager = _newRootChainManager;
     }
 
-    function updateErc20PolygonManager(address _erc20PolygonManager)
-        external
-        onlyOwner
-    {
-        require(
-            _erc20PolygonManager != address(0),
-            "Bad ERC20PolygonManager address"
-        );
+    function updateErc20PolygonManager(address _erc20PolygonManager) external onlyOwner {
+        require(_erc20PolygonManager != address(0), "Bad ERC20PolygonManager address");
         erc20PolygonManager = _erc20PolygonManager;
+    }
+
+    function updateErc20Polygon(address _erc20Polygon) external onlyOwner {
+        require(_erc20Polygon != address(0), "Bad ERC20Polygon address");
+        erc20Polygon = _erc20Polygon;
     }
 }
