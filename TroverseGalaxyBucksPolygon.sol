2f466c89c4e2a12a1f982e26a4394b495ba2fbe7
TroverseDev <troversenft@gmail.com>
2022-06-23 14:56:16 -0700

    Rename (ERC20Polygon) : burn function

--- TroverseGalaxyBucksPolygon.sol
+++ TroverseGalaxyBucksPolygon.sol
@@ -59,7 +59,7 @@ contract TroverseGalaxyBucksPolygon is ERC20, Ownable {
         _transfer(manager, _to, _amount);
     }
 
-    function burnByTransfer(address _from, uint256 _amount) external onlyOperator {
+    function burn(address _from, uint256 _amount) external onlyOperator {
         _transfer(_from, manager, _amount);
     }
 
